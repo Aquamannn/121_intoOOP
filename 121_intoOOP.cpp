31e71abe67a049b04a93814ea909efa6261a6bc3
Lenovo <9.3razzinayyuri@gmail.com>
2024-04-22 17:53:19 +0700

    input class mk

--- 121_intoOOP.cpp
+++ 121_intoOOP.cpp
@@ -13,3 +13,24 @@ class mahasiswa {
 			cout << "Jurusan : " << jurusan << endl;
 		}
 };
+
+class Matakuliah {
+	private:
+		string kodemk;
+		string namamk;
+		int sks;
+	public:
+		void input() {
+			cout << "Kode MK: ";
+			cin >> kodemk;
+			cout << "Nama MK: ";
+			cin >> namamk;
+			cout << "SKS: ";
+			cin >> sks;
+		}
+		void output() {
+			cout << "Kode MK : " << kodemk << endl;
+			cout << "Nama MK: " << namamk << endl;
+			cout << "SKS : " << sks << endl;
+		}
+};
