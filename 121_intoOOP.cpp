fe94255f8cd1fbe316b7bf8f6a08c546e16df029
Lenovo <9.3razzinayyuri@gmail.com>
2024-04-22 17:46:46 +0700

    input public

--- 121_intoOOP.cpp
+++ 121_intoOOP.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 class mahasiswa {
 	public: // akses modifier
+		string nama;
+		int umur;
+		string jurusan;
 
+		void output() {
+			cout << "Nama : " << nama << endl;
+			cout << "Umur : " << umur << endl;
+			cout << "Jurusan : " << jurusan << endl;
+		}
 };
